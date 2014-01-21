719da5325c60a57c35ab6a90161ef92a7798abcb
Alf Eaton <alf@hubmed.org>
2014-01-21 11:39:19 +0000

    insert html into the dom so that itemref ids can be found

--- microdata.js
+++ microdata.js
@@ -1,16 +1,19 @@
 $(function() {
 	var input = $('textarea');
 	var output = $('output');
+	var container = $('#container');
 
-	input.on('input', function() {
-		var html = $('<html>' + input.val() + '</html>');
+	var update = function() {
+		container.html(input.val());
 
-		var subitems = html.find('[itemscope] [itemscope]');
-		var items = html.find('[itemscope]').not(subitems);
+		var subitems = container.find('[itemscope] [itemscope]');
+		var items = container.find('[itemscope]').not(subitems);
 
 		var data = items.microdata();
 		var json = JSON.stringify(data, null, 2);
 
 		output.text(json);
-	});
+	};
+
+	input.on('input', update).trigger('input');
 });
